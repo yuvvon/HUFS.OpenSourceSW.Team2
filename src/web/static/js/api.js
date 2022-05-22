e916aada063b1c3b074af64db29b18338af28dcb
jhchoi123 <solid2113@naver.com>
2022-05-22 21:06:14 +0900

    add bootstrap icons

--- src/web/static/js/api.js
+++ src/web/static/js/api.js
@@ -2,7 +2,7 @@
  * API root url
  * @type {string}
  */
-const API_ROOT = "http://localhost:3000"
+const API_ROOT = "http://127.0.0.1:5000"
 
 /**
  * api endpoints
@@ -19,6 +19,35 @@ const API_URL = {
         method: "GET"
     }
 }
+
+
+const TEST_DATA = {
+    fetchColorCombiRes: {
+        pickedLoc: "TOP",
+        pickedColor: "WHITE",
+        colors: [
+            "LIGHTBLUE",
+            "BEIGE"
+        ],
+        colorCode: [
+            { name: "WHITE", code: "ffffff" },
+            { name: "LIGHTBLUE", code: "add8e6" },
+            { name: "BEIGE", code: "f5f5dc" }
+        ]
+    },
+    fetchColorListRes: {
+        list: [
+            { top: "WHITE", BOTTOM: "LIGHTBLUE" },
+            { top: "WHITE", BOTTOM: "BEIGE"}
+        ],
+        colorCode: [
+            { name: "WHITE", code: "ffffff" },
+            { name: "LIGHTBLUE", code: "add8e6" },
+            { name: "BEIGE", code: "f5f5dc" }
+        ]
+    }
+};
+
 
 /**
  * check loc is correct. loc must be "TOP" or "BOTTOM".
@@ -40,14 +69,15 @@ const fetchColorCombi = async (color, loc) => {
     if (!isCorrectLoc(loc)) {
         throw new Error(`${loc} is Invalid Loc`);
     }
-    const endpoint = API_URL.combi;
+    /*const endpoint = API_URL.combi;
 
     const url = `${API_ROOT}${endpoint.path}?color=${color}&loc=${loc}`;
     const res = await fetch(url, {
         method: endpoint.method
     });
 
-    return res.json();
+    return res.json();*/
+    return TEST_DATA.fetchColorCombiRes;
 }
 
 /**
@@ -56,10 +86,11 @@ const fetchColorCombi = async (color, loc) => {
  * @async
  */
 const fetchColorList = async () => {
-    const endpoint = API_URL.list;
+    /*const endpoint = API_URL.list;
     const url = `${API_ROOT}${endpoint.path}`;
     const res = await fetch(url, {
         method: endpoint.method
     });
-    return res.json();
+    return res.json();*/
+    return TEST_DATA.fetchColorListRes;
 }
--- src/web/static/js/index.js
+++ src/web/static/js/index.js
@@ -1,7 +1,58 @@
+const getElem = (querySelector) => {
+    return document.querySelector(querySelector);
+};
+
+// html elements
+const partTopButton = getElem("#main-part-top-button");
+const partBottomButton = getElem("#main-part-bottom-button");
+const choosePartDiv = getElem("#choose-part-div");
+const colorPaletteDiv = getElem("#main-part-color-palette");
+
+const hideElem = (elem) => {
+    elem.className += " hide-with-animation";
+    setTimeout(() => {
+        elem.style.display = "hide";
+    }, 1000);
+}
+
+const showElem = (elem) => {
+    elem.style.display = "block";
+    setTimeout(() => {
+        elem.className = elem.className.replace("hide-with-animation", "show-with-animation");
+    }, 1000);
+}
+
+const createColorPalette = (colorList, codeList) => {
+
+}
+
 // Run Async Code Here
 (async () => {
     try {
+        // fetch color combi list
         const colorList = await fetchColorList();
+
+        const partSelectOnClick = (loc) => {
+            hideElem(choosePartDiv);
+
+            let sortColorList
+            if (loc === "TOP") {
+                sortColorList = colorList.list.sort((prev, next) => {
+                   return prev.top - next.top;
+                });
+            } else {
+
+            }
+
+
+            showElem(colorPaletteDiv);
+        };
+
+        // add onclick event to part select button
+        partTopButton.onclick = () => { partSelectOnClick("TOP"); };
+        partBottomButton.onclick = () => { partSelectOnClick("BOTTOM"); };
+
+
     } catch (e) {
         console.log(e);
     }
