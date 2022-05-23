f4085ae837576dba7542b46b4c0e178583c4ff8f
Yun <77044214+luckyseo@users.noreply.github.com>
2022-05-23 19:25:45 +0900

    route color api구현  (#11)
    implement router

--- src/main.py
+++ src/main.py
@@ -1,7 +1,15 @@
-from flask import Flask
-from color import get_combi, get_color_code
 
+from flask import Flask, request, jsonify
+from color import get_combi, get_color_code,combinations,color_code_map
+
+"""
+ref  https://velog.io/@jewon119/01.Flask-%EA%B8%B0%EC%B4%88-REST-API-%EA%B5%AC%ED%98%84
+"""
 app = Flask(__name__, static_url_path="", static_folder="web/static")
+
+def rgb_to_hex(r,g,b):
+    r,g,b = int(r),int(g), int(b)
+    return hex(r)[2:].zfill(2) + hex(g)[2:].zfill(2) + hex(r)[2:].zfill(2)
 
 
 @app.route("/")
@@ -18,21 +26,45 @@ def color_page():
 
 @app.route("/api/color/combi")
 def color_combi():
-    color = ""
-    loc = ""
+    color = request.args.get("color")
+    loc = request.args.get("loc")
 
-    combi = get_combi(loc, color)
-    color_code = get_color_code(combi)
-    # Call "get_combi" Function
-    return "RESULT_OF_GET_COMBI_WILL_BE_RETURN"
-
+    combi = get_combi(loc, color) #상의 또는 하의 어울리는 color -> list로 리턴
+    combi = set(combi)
+   # color_code = get_color_code(combi) #param changed combi -> color (get_color_code 함수가 color를 받습니다)
+    color_code_dic=[]
+    for c in combi:
+        color_code = get_color_code(c)
+        r,g,b= color_code[0],color_code[1],color_code[2]
+        temp = {"name": c, "code":rgb_to_hex(r,g,b)}
+        color_code_dic.append(temp)
+    combi_db={
+        "pickedColor":color,
+        "pickedLoc":loc,
+        "colors":combi,
+        "colorCode":color_code_dic
+    }
+    return jsonify(combi_db) 
 
 # TODO: Implement color combinations API Route
 
 
 @app.route("/api/color/list")
 def color_list():
-    return "RETURN_ALL_COLORS_LIST"
+    list_db=[]
+    for c in combinations:
+        list_db.append({"top":c[0], "bottom":c[1]})
+        
+    color_code_db=[]
+    for c in color_code_map:
+        r,g,b= color_code_map[0],color_code_map[1],color_code_map[2]
+        temp = {"name": c, "code":rgb_to_hex(r,g,b)}
+        color_code_db.append(temp)
+    combi_data={
+        "list":list_db,
+        "colorCode":color_code_db,
+    }
+    return jsonify(combi_data)
 
 
 # TODO: Implement color list API Route
